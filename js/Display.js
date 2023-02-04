53c6b0fdc16716a3551519c2ec1e717f682bbce2
nazapasa <nazaarismendi.99@gmail.com>
2023-02-03 20:12:46 -0500

    subir proyecto

---
+++ js/Display.js
@@ -0,0 +1,51 @@
+class Display{
+    constructor(displayValorAnterior, displayValorActual){
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+    }
+
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+
+    }
+
+    computar(tipo){
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+
+    }
+
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = ``;
+    }
+
+    calcular(){
+        const valorAnterior = parseFloar(this.valorAnterior);
+        const valorActual = parseFloar(this.valorActual);
+
+        if(isNan(valorActual) || isNan(valorAnterior)) return
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+    }
+}
